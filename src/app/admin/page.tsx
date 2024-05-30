26fc28958f8c42f259034bc8f696f91e6f451786
Brandon Stone <brandonrstone@gmail.com>
2024-05-29 21:43:07 -0400

    Added formatter to lib

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,50 @@
-import { Card } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 
-export default function AdminDashboard() {
+import db from '@/db/db';
+
+export default async function AdminDashboard() {
+  const salesData = await getSalesData()
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-      <Card />
+      <DashboardCard
+        title='Sales'
+        subtitle={salesData.numberOfSales}
+        body={salesData.amount}
+      />
     </div>
+  )
+}
+
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true
+  })
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count
+  }
+}
+
+type DashboardCardProps = {
+  title: string
+  subtitle: string
+  body: string
+}
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
   )
 }
---
+++ src/db/db.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client'
+
+// Entry point for SQLite
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare const globalThis: {
+  prisma: ReturnType<typeof prismaClientSingleton>
+} & typeof global
+
+const db = globalThis.prisma ?? prismaClientSingleton()
+
+export default db
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db
